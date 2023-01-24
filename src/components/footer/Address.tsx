ab6274cd42f9d410c646f4cef5abb3e7a6430c1b
Igor Stepanov <stipyk1309@gmail.com>
2023-01-24 19:43:17 +0300

    footer Address component add yandex map

--- src/components/footer/Address.tsx
+++ src/components/footer/Address.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import { YMaps, Map } from '@pbe/react-yandex-maps';
 import { Typography } from '../common';
 
 const AddressWrapper = styled.div`
+  filter: grayscale(1);
+  -ms-filter: grayscale(1);
+  -webkit-filter: grayscale(1);
+  -moz-filter: grayscale(1);
+  -o-filter: grayscale(1);
   address {
     padding: 0 0 20px;
     color: #ffffff;
@@ -18,5 +24,17 @@ export const Address: React.FC = () => (
     <address>
       Sitio do Escampadinho, Mexilhoeira Grande, 8500-148 Portimao, Portugal
     </address>
+    <YMaps>
+      <Map
+        defaultState={{
+          center: [37.23142279396843, -8.628370515477373],
+          zoom: 14,
+        }}
+        modules={['geolocation', 'geocode']}
+        width="100%"
+        height="75%"
+	
+      />
+    </YMaps>
   </AddressWrapper>
 );
