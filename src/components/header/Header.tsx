2349729b6958a413af93fe07b5e4398bdc65db90
Igor Stepanov <stipyk1309@gmail.com>
2023-01-25 23:27:30 +0300

    adaptive Header and Navigation components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,9 +8,12 @@ const StyledHeader = styled.header`
   position: absolute;
   left: 0;
   right: 0;
-  padding: 25px 62px 0 0;
+  padding: 25px 42px 0 0;
   background: ${theme.colors.gradient.promo};
   filter: ${theme.colors.filter.promoFilter};
+  ${theme.breakpoints.mobile} {
+    padding: 25px 10px;
+  }
 `;
 
 export const Header: React.FC = () => (
